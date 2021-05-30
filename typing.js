8058e7fa3468f0198310832267fb1e2269b46a66
hackathonlukky <85036118+hackathonlukky@users.noreply.github.com>
2021-05-30 19:18:32 +0530

    Add files via upload

---
+++ typing.js
@@ -0,0 +1,10 @@
+var typed =new Typed(".typed",{
+  strings: ["Cultur.", "A new meaning to college life.", "Add any text you like here."],
+  typeSpeed: 100,
+  startDelay: 5,
+  backSpeed: 60,
+  backDelay: 2000,
+  loop: true,
+  cursorChar: "|",
+  contentType: 'html'
+});
